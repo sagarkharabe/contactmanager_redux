dd8199a837c3d01051b4bfda3cd2bc62290450f0
RaghunathVadlakonda <sagarkharabe97@gmail.com>
2019-02-27 15:37:56 +0530

    init

---
+++ src/actions/contactActions.js
@@ -0,0 +1,54 @@
+import {
+  GET_CONTACTS,
+  DELETE_CONTACTS,
+  ADD_CONTACTS,
+  GET_CONTACT,
+  UPDATE_CONTACT
+} from "./types";
+import Axios from "axios";
+
+export const getContacts = () => async dispatch => {
+  const res = await Axios.get("https://jsonplaceholder.typicode.com/users");
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
+};
+export const updateContact = contact => async dispatch => {
+  const res = await Axios.put(
+    `https://jsonplaceholder.typicode.com/users/${contact.id}`,
+    contact
+  );
+  dispatch({
+    type: UPDATE_CONTACT,
+    payload: res.data
+  });
+};
+
+export const getContact = id => async dispatch => {
+  const res = await Axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  dispatch({
+    type: GET_CONTACT,
+    payload: res.data
+  });
+};
+
+export const deleteContacts = id => async dispatch => {
+  await Axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  dispatch({
+    type: DELETE_CONTACTS,
+    payload: id
+  });
+};
+export const addContacts = contact => async dispatch => {
+  const res = await Axios.post(
+    `https://jsonplaceholder.typicode.com/users`,
+    contact
+  );
+  dispatch({
+    type: ADD_CONTACTS,
+    payload: res.data
+  });
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import contactReducer from "./contactReducers";
+export default combineReducers({
+  contact: contactReducer
+});
